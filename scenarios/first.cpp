52b779c161330a282acbf680fb36e291a991fb28
belight2 <chlwnsgur090@gmail.com>
2024-05-30 17:17:49 +0900

    reset scenarios cpp file

--- scenarios/first.cpp
+++ scenarios/first.cpp
@@ -8,16 +8,7 @@
 
 #define ECHO_PORT 3000
 
-#define _CRTDBG_MAP_ALLOC
-#include <cstdlib>
-#include <crtdbg.h>
-
-#ifdef _DEBUG
-#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
-#endif
-
 int main() {
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
   // ---------- //
   // 토폴로지 설정 //
   // ---------- //
